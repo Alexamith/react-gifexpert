a934bcca7513d17d186a14511f5e6c6be7ab8bfd
THLDevelop <desarrollo@transporteshl.com>
2022-03-23 11:31:02 -0600

    Second deploy

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async(category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=gWTAM39HlVx76NdUpDDWnC9feXka5PPi`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=12&api_key=gWTAM39HlVx76NdUpDDWnC9feXka5PPi`;
     const res = await fetch(url);
     const {data} = await res.json();
 
